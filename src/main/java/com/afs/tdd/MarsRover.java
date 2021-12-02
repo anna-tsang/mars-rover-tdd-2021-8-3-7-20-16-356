612d4989a11dab4579f465a5a575285a0c9070c8
ANNA TSANG <TSANGAN@OOCL.COM>
2021-12-02 16:20:17 +0800

    [marsRover][Anna] feat: command M - direction N - location Y should add one

--- src/main/java/com/afs/tdd/MarsRover.java
+++ src/main/java/com/afs/tdd/MarsRover.java
@@ -1,4 +1,13 @@
 package com.afs.tdd;
 
 public class MarsRover {
+    private final String locationX;
+    private final String locationY;
+    private final String direction;
+
+    public MarsRover(String locationX, String locationY, String direction){
+        this.locationX = locationX;
+        this.locationY = locationY;
+        this.direction = direction;
+    }
 }
--- src/test/java/com/afs/tdd/DemoTest.java
+++ src/test/java/com/afs/tdd/DemoTest.java
@@ -2,9 +2,17 @@ package com.afs.tdd;
 
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 class MarsRoverTest {
     @Test
-    void write_your_first_test() {
+    void should_locationY_add_one_when_execute_given_direction_N_and_command_M(){
+        MarsRover marsRover = new MarsRover("0","0","N");
+        String command = "M";
 
+        marsRover.executeCommand(command);
+        String result = marsRover.getStatus();
+
+        assertEquals("0 1 N", result);
     }
 }
