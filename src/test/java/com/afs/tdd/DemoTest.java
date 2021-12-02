20f0da5b96eeda47ff3b35226474a8e08c7f76c2
ANNA TSANG <TSANGAN@OOCL.COM>
2021-12-02 16:32:24 +0800

    [marsRover][Anna] feat: command M - direction E - location X should add one

--- src/test/java/com/afs/tdd/DemoTest.java
+++ src/test/java/com/afs/tdd/DemoTest.java
@@ -26,4 +26,15 @@ class MarsRoverTest {
 
         assertEquals("0 -1 S", result);
     }
+
+    @Test
+    void should_locationY_minus_one_when_execute_given_direction_E_and_command_M(){
+        MarsRover marsRover = new MarsRover(0,0,"E");
+        String command = "M";
+
+        marsRover.executeCommand(command);
+        String result = marsRover.getStatus();
+
+        assertEquals("1 0 E", result);
+    }
 }
